b0f5b2e8c7c8bcd310b36ab7e6f6d4bcdda7236c
DtRWoS <work@dtrwos.com>
2015-12-02 13:02:08 -0800

    initial token negotiation succedding now to extract from response object

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,35 +1,33 @@
 class UserController < ApplicationController
-=begin
-Consumer Key (API Key)	HIDYtyLfPnPjZEjSgJT4SdHw4
-Consumer Secret (API Secret)	nFwwfQPYhDqvd9ErZDYPN4il9Zu1JWqqqoG8lmUbwQW4xvitww
-=end
+require "http"
+@activeToken
 
   def index
-    get_tweets '@trueX'
+    puts get_tweets(userid: '@trueX')
   end
 
   def byid
   end
 
 protected
-  def get_tweets
-    token
+  def get_tweets(userid: nil)
+    activeToken ||= token
+    activeToken.to_a
     #https://api.twitter.com/1.1/statuses/user_timeline.json
   end
 
 
-  def token(options = {})
+  def token(options = {}, body = {})
     key    = 'HIDYtyLfPnPjZEjSgJT4SdHw4'
     secret = 'nFwwfQPYhDqvd9ErZDYPN4il9Zu1JWqqqoG8lmUbwQW4xvitww'
-    token = 'Basic ' + Base64.encode64(key + ':' + secret)
-    options[:authorization] = token
-    options[:Content-Type]  = 'application/x-www-form-urlencoded;charset=UTF-8'
+    token = 'Basic ' + Base64.strict_encode64(key + ':' + secret)
+    #options[:authorization] = token
+    #options[:Content-Type]  = 'application/x-www-form-urlencoded;charset=UTF-8'
     options[:accept]        = '*/*'
-    body[:grant_type]  ||= 'client_credentials'
+    body[:grant_type]       = 'client_credentials'
     url = 'https://api.twitter.com/oauth2/token'
-    headers = Twitter::Headers.new(self, :post, url, options).request_headers
-      response = HTTP.with(options).post(url, form: body)
-      response.parse['access_token']
-    end
-
+    response = HTTP.auth(token).headers(options).post(url, form: body).to_a
+    #response = HTTP.with(options).post(url, form: body)
+    #response #.parse['access_token']
+  end
 end
